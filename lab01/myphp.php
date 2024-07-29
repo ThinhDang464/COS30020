20c63a6640e267312e7d115805d8b804dd7005de
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2024-07-29 18:27:00 +1000

    lab 1 commit

---
+++ lab01/myphp.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    <h1>Web Programming - Lab 1</h1>
+    <p>Can I create a .php file that do not contain any PHP scripts?</p>
+    <p>Yes, but this should be avoided, so that the server does not do any
+    unnecessary parsing and processing.</p>
+</body>
+</html>
